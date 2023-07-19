a3e5c260bf125dd5b372b2c6827baf3497522bd4
B.dev <56345098+basit333@users.noreply.github.com>
2023-07-19 05:00:58 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 /*
 ---------------------------------------
+            Navbar
+---------------------------------------
+*/
+
+const navOpenBtn = document.querySelector(".nav__bar--hamburger-btn");
+const navCloseBtn = document.querySelector(".mobile__navbar--close-btn");
+const navMobileMenu = document.querySelector(".mobile__navbar");
+
+if (navOpenBtn) {
+  navOpenBtn.addEventListener("click", function () {
+    navMobileMenu.classList.add("mobile__navbar--open");
+  });
+}
+
+if (navCloseBtn) {
+  navCloseBtn.addEventListener("click", function () {
+    navMobileMenu.classList.remove("mobile__navbar--open");
+  });
+}
+
+/*
+---------------------------------------
             Grid Slider
 ---------------------------------------
 */
@@ -34,20 +56,56 @@ new Swiper(".mySwiperGrid", {
 */
 
 const videoPlayBtn = document.querySelector(".video__play--btn");
-const videoOverlayImg = document.querySelector(".video__overlay--img");
+// const videoOverlayImg = document.querySelector(".video__overlay--img");
 const videoPlayer = document.querySelector(".video__player");
 
 // Hide video controls by default
 videoPlayer.controls = false;
 
 videoPlayBtn.addEventListener("click", () => {
-  videoOverlayImg.classList.add("video__overlay--img-hidden");
+  // videoOverlayImg.classList.add("video__overlay--img-hidden");
   videoPlayBtn.classList.add("video__play--btn-hidden");
 
   setTimeout(() => {
-    videoOverlayImg.style.display = "none";
+    // videoOverlayImg.style.display = "none";
     videoPlayBtn.style.display = "none";
     videoPlayer.controls = true; // Show video controls
     videoPlayer.play(); // Play the video
   }, 500);
+});
+
+/*
+---------------------------------------
+                FAQ
+---------------------------------------
+*/
+// Get DOM elements
+const faqQuestionWrappers = document.querySelectorAll(".faq__question--wrapper");
+const faqAnswerWrappers = document.querySelectorAll(".faq__answer--wrapper");
+const faqPlusIcons = document.querySelectorAll(".faq-plus-icon");
+const faqMinusIcons = document.querySelectorAll(".faq-minus-icon");
+
+// Add event listeners to question wrappers
+faqQuestionWrappers.forEach((questionWrapper, index) => {
+  questionWrapper.addEventListener("click", () => {
+    // Remove "faq__answer--wrapper-open" class from all answer wrappers
+    faqAnswerWrappers.forEach((answerWrapper, i) => {
+      if (i !== index) {
+        answerWrapper.classList.remove("faq__answer--wrapper-open");
+      }
+    });
+
+    // Toggle visibility of the corresponding answer wrapper
+    faqAnswerWrappers[index].classList.toggle("faq__answer--wrapper-open");
+    const isOpen = faqAnswerWrappers[index].classList.contains("faq__answer--wrapper-open");
+
+    // Toggle classes of plus and minus icons
+    faqPlusIcons.forEach((plusIcon, i) => {
+      plusIcon.style.display = i === index && isOpen ? "none" : "block";
+    });
+
+    faqMinusIcons.forEach((minusIcon, i) => {
+      minusIcon.style.display = i === index && isOpen ? "block" : "none";
+    });
+  });
 });
